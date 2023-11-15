317fb08214ef2d5ac6461277bef8ffd8a9facd77
3x3ltv <makkanygin@gmail.com>
2023-11-15 09:48:44 +0300

    Ответ координатами по новой

--- main.py
+++ main.py
@@ -12,34 +12,21 @@ def get_updates(offset=0):
 def send_message(chat_id, text):
     requests.get(f'{URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
 
-def reply_keyboard(chat_id, message):
-    if 'location' in message:
-        # If the user sent a location, retrieve the coordinates
-        latitude = message['location']['latitude']
-        longitude = message['location']['longitude']
-        response_text = f'Твои координаты: {latitude}, {longitude}'
-        send_message(chat_id, response_text)
-    else:
-        # If it's not a location, respond with the keyboard
-        reply_markup = {
-            "keyboard": [["Привет", "Hello"]],
-            "resize_keyboard": True,
-            "one_time_keyboard": True
-        }
-        data = {'chat_id': chat_id, 'text': 'Я не понимаю тебя :(', 'reply_markup': json.dumps(reply_markup)}
-        requests.post(f'{URL}{TOKEN}/sendMessage', data=data)
+def reply_keyboard(chat_id, location):
+    latitude = location['latitude']
+    longitude = location['longitude']
+    response_text = f'Твои координаты: {latitude}, {longitude}'
+    send_message(chat_id, response_text)
 
 def check_message(chat_id, message):
-    if (user_message := message.get('text')):
-        check_location = message.get('location')
-        if check_location:
-            reply_keyboard(chat_id, message)
-        elif user_message.lower() in ['привет', 'hello']:
+    if 'text' in message:
+        user_message = message['text'].lower()
+        if user_message in ['привет', 'hello']:
             send_message(chat_id, 'Привет :)')
         else:
             reply_keyboard(chat_id, message)
-    else:
-        send_message(chat_id, 'Не пон :)')
+    elif 'location' in message:
+        reply_keyboard(chat_id, message)
 
 def run():
     update_id = get_updates()[-1]['update_id'] # Сохраняем ID последнего отправленного сообщения боту
