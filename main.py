eb91b19e0d0b710d0904d25f782eb1202612cd66
3x3ltv <makkanygin@gmail.com>
2023-11-15 13:50:44 +0300

    Теперь всё на библиотеке python telegram-bot 13 версии

--- main.py
+++ main.py
@@ -20,7 +20,31 @@ def handle_location(update: Update, context: CallbackContext) -> None:
     location = update.message.location
     latitude = location.latitude
     longitude = location.longitude
-    update.message.reply_text(f'Твои координаты: {latitude}, {longitude}')
+
+    # Базовые значения координат
+    base_coordinates = [
+        {"name": "Москва", "latitude": 55.7558, "longitude": 37.6176},
+        {"name": "Санкт-Петербург", "latitude": 59.9343, "longitude": 30.3351},
+        # Добавьте другие базовые значения координат при необходимости
+    ]
+
+    closest_location = None
+    min_distance = float('inf')
+
+    for base_coord in base_coordinates:
+        base_latitude = base_coord["latitude"]
+        base_longitude = base_coord["longitude"]
+
+        # Рассчитываем расстояние между базовой точкой и точкой пользователя
+        distance = ((latitude - base_latitude)**2 + (longitude - base_longitude)**2)**0.5
+
+        # Если расстояние меньше, чем минимальное до этого, обновляем ближайшую локацию
+        if distance < min_distance:
+            min_distance = distance
+            closest_location = base_coord["name"]
+
+    # Возвращаем ответ с учетом ближайшей локации
+    update.message.reply_text(f'Твои координаты: {latitude}, {longitude}. Ближайшее место: {closest_location}')
 
 def main() -> None:
     updater = Updater(TOKEN)
