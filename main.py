78828bfb07a2f32689c5c00cafb6b1af5cc105f1
3x3ltv <makkanygin@gmail.com>
2023-11-15 09:14:19 +0300

    main переделан для теста

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
-from telegram import Location
+import requests
+import time
+import json
 
-# Замените 'YOUR_BOT_TOKEN' на фактический токен вашего бота
 TOKEN = '6721006067:AAEpHivlsux5MYKh49UdWdrNC9KGwFT5nGQ'
+URL = 'https://web.telegram.org/k/#@PetrSide_bot'
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Этот бот реагирует на отправленную геолокацию.')
+def get_updates(offset=0):
+    result = requests.get(f'{URL}{TOKEN}/getUpdates?offset={offset}').json()
+    return result['result']
 
-def location_handler(update: Update, context: CallbackContext) -> None:
-    user_location = update.message.location
-    latitude = user_location.latitude
-    longitude = user_location.longitude
+def send_message(chat_id, text):
+    requests.get(f'{URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
 
-    # Пример: создаем текстовое сообщение на основе координат
-    response_message = f'Вы находитесь на координатах: {latitude}, {longitude}'
+def reply_keyboard(chat_id, text):
+    reply_markup ={ "keyboard": [["Привет", "Hello"], [{"request_location":True, "text":"Где я нахожусь"}]], "resize_keyboard": True, "one_time_keyboard": True}
+    data = {'chat_id': chat_id, 'text': text, 'reply_markup': json.dumps(reply_markup)}
+    requests.post(f'{URL}{TOKEN}/sendMessage', data=data)
 
-    update.message.reply_text(response_message)
+def check_message(chat_id, message):
+    if message.lower() in ['привет', 'hello']:
+        send_message(chat_id, 'Привет :)')
+    else:
+        reply_keyboard(chat_id, 'Я не понимаю тебя :(')
 
-def main() -> None:
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
-
-    # Обработка команды /start
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    # Обработка геолокации
-    dispatcher.add_handler(MessageHandler(filters.Location, location_handler))
-
-    # Запускаем бота
-    updater.start_polling()
-
-    # Ожидаем завершение работы бота (например, нажатие Ctrl+C)
-    updater.idle()
+def run():
+    update_id = get_updates()[-1]['update_id'] # Сохраняем ID последнего отправленного сообщения боту
+    while True:
+        time.sleep(2)
+        messages = get_updates(update_id) # Получаем обновления
+        for message in messages:
+            # Если в обновлении есть ID больше чем ID последнего сообщения, значит пришло новое сообщение
+            if update_id < message['update_id']:
+                update_id = message['update_id']# Сохраняем ID последнего отправленного сообщения боту
+                if (user_message := message['message'].get('text')): # Проверим, есть ли текст в сообщении
+                    check_message(message['message']['chat']['id'], user_message) # Отвечаем
+                if (user_location := message['message'].get('location')): # Проверим, если ли location в сообщении
+                    print(user_location)
 
 if __name__ == '__main__':
-    main()
+    run()
